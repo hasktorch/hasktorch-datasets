3adfbe37115c2cf7263081706ccdffc884ae91e0
Junji Hashimoto <junji.hashimoto@gmail.com>
2021-09-15 03:08:42 +0000

    Add mAP and AP with labels

--- analysis/calc-map.nix
+++ analysis/calc-map.nix
@@ -9,19 +9,27 @@ let getValues = attrs:
     datasets_list_str = builtins.concatStringsSep " " (
       builtins.map (n: builtins.toString(n.out)) datasets_list
     );
+    headAttr = f: attrs:
+      let name = builtins.head (builtins.attrNames attrs);
+      in f name attrs."${name}";
+    forEach = f: attrs: lib.concatStrings (lib.mapAttrsToList f attrs);
 in
   stdenv.mkDerivation rec {
     pname = "calc-map";
     version = "0.1";
     nativeBuildInputs = datasets_list ++ [jq];
-    
     installPhase = ''
       mkdir -p $out
-      for i in ${datasets_list_str}; do
-        echo "---"$i"---" >> $out/report.md
-        grep all $i/test.log >> $out/report.md 
-        jq . < $i/map_results.json >> $out/report.md
-      done
-    '';
+      echo "#datasets,mAP@.5" >> $out/mAP.csv
+    '' + headAttr (name: value: ''
+      echo -n "datasets," >> $out/AP.csv
+      grep names: ${value.out}/dataset.test.yaml  | sed -e "s/'/\"/g" -e 's/names://g' | jq "@csv" -r >> $out/AP.csv
+    '') datasets
+    + forEach (name: value: ''
+      echo -n "${name}," >> $out/mAP.csv
+      jq '.["mAP@.5"]' -r < ${value.out}/map_results.json >> $out/mAP.csv
+      echo -n "${name}," >> $out/AP.csv
+      jq '.["AP@.5"] | @csv' -r < ${value.out}/map_results.json >> $out/AP.csv
+    '') datasets;
     phases = [ "installPhase" ];
   }
